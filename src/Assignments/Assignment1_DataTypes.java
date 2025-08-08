32823f1d82027ad0f24d3970748314be3efb81de
Abdul Rahman <abdulrahmaan065@gmail.com>
2025-08-09 01:18:00 +0530

    Assignment 1

---
+++ src/Assignments/Assignment1_DataTypes.java
@@ -0,0 +1,91 @@
+package Assignments;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+	
+		
+ //Temperature of a city in degrees Celsius: 25.5
+ float Temperature = 25.5f;
+ System.out.println(Temperature);
+ 
+ //Whether a customer has placed an order: true or false
+ boolean order = true;
+ System.out.println(order);     
+                  
+ //Person's phone number: "123-456-7890"
+ long phno = 1234567890l;
+ System.out.println(phno);   
+ 
+ //Amount of money in a customer's bank account: 1000.50
+ float BA = 1000.51f;
+ System.out.println(BA);
+ 
+ //Person's email address: "john.doe@example.com"
+ String emailaddress = "john.doe@example.com";
+ System.out.println(emailaddress);
+ 
+ //Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+ double latitude = 37.7749d;
+ double	 longitude = -122.4194d;
+ System.out.println(latitude);
+ System.out.println(longitude);
+ 
+ //Person's marital status: true or false
+ boolean Maritalstatus = false;
+ System.out.println(Maritalstatus);
+ 
+ //Person's occupation: "Software Engineer"
+ String occupation = "Software Engineer";
+ System.out.println(occupation);
+  
+ //Person's favourite colour: "Blue"
+ String colour = "Blue";
+ System.out.println(colour);
+ 
+ //Current year: 2023
+ short year = 2023;
+ System.out.println(year);
+ 
+ //Number of followers on a social media platform: 1,000,000
+ String SocialMedia = "1,000,000";
+ System.out.println(SocialMedia);
+ 
+ //Rating of a movie: 7.5
+ float Rating = 7.5f;
+ System.out.println(Rating);
+ 
+ //Person's blood type: 'A'
+ char blood = 'A';
+ System.out.println(blood);
+ 
+ //Title of a book: "To Kill a Mockingbird"
+ String book = "To Kill a Mockingbird";
+ System.out.println(book);
+ 
+ //Number of employees in a company: 500
+ short employees = 500;
+ System.out.println(employees);
+ 
+ //Time of an event: 2:30 PM
+ String time = "2:30 PM";
+ System.out.println(time);
+  
+ //Name of a country: "United States"
+ String country = "United States";
+ System.out.println(country);
+ 
+ //Person's eye color: "Brown"
+ String eye = "Brown";
+ System.out.println(eye);
+ 
+ //Person's birthplace: "New York City"
+ String birthplace = "New York City";
+ System.out.println(birthplace); 
+ 
+//Distance between two cities: 200.5
+ float Distance = 200.5f;
+ System.out.println(Distance);
+	}
+
+}
